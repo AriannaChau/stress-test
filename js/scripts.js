50f8031de92ad0a02eaec638782888b072f61f1c
Arianna Chau and Steven Galvin <arianna@>
2017-03-29 20:40:18 -0700

    add two more arrays to make code functional.

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,35 @@
 // front end logic
 $(document).ready(function() {
+  var total;
   $("form#stress-test").submit(function(event) {
     event.preventDefault();
-    // var warningTotal;
     var warningArray = [];
-    var userResponses = [];
+    var symptomsArray = [];
+    var copingArray = [];
 
     $("input:checkbox[name=warning-signs]:checked").each(function() {
       var warningSigns = $(this).val();
       warningArray.push(warningSigns);
-      // warningTotal = warningArray.length;
     });
-    console.log(warningTotal);
+
     $("input:checkbox[name=symptoms]:checked").each(function() {
       var symptoms = $(this).val();
+      symptomsArray.push(symptoms);
     });
+
     $("input:checkbox[name=coping]:checked").each(function() {
       var coping = $(this).val();
+      copingArray.push(coping);
     });
- // warningarray.length + symptons.length - coping.length
-  
-    //   if (warningSigns >=3 && symptoms >=2 && coping <=2) {
-    //     $("#lowCoping").show();
-    //   } else if (warningSigns >=3 && symptoms >=2 && coping >=5) {
-    //     $("#highCoping").show();
-    //   } else (warningSigns <=2 && symptoms <=2 &&>=4){
-    //     $("#lowStress").show();
-    //   }
-    // }
+
+    total = warningArray.length + symptomsArray.length - copingArray.length
+
+    if (total <= 6) {
+      $("#lowStress").show();
+    } else if (total <= 9) {
+      $("#highCoping").show();
+    } else {
+      $("#lowCoping").show();
+    }
   });
 });
